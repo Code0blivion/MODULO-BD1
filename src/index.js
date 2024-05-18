82e34bc4d03da4ba43170d415d4e7f1875eec06b
Code0blivion <bautistaluis1002@gmail.com>
2024-05-18 15:40:37 -0500

    Setup Inicial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
