910f3d75986c6f8ed652b1f286c4bfd439106fff
Code0blivion <bautistaluis1002@gmail.com>
2024-05-20 21:51:03 -0500

    Lista y detalle reqerimientos

--- src/index.js
+++ src/index.js
@@ -10,6 +10,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/static/templates/index.html"));
 });
 
+app.get("/lista_requerimientos", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/static/templates/lista_requerimientos.html")
+  );
+});
+
+app.get("/req", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/static/templates/detalle_requerimiento.html")
+  );
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
