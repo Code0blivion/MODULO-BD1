95fe678d7716025ad5e609596ea091b60d74124f
Code0blivion <bautistaluis1002@gmail.com>
2024-05-23 21:05:17 -0500

    Adición Vista Fase 7

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ app.get("/req", (req, res) => {
   );
 });
 
+app.get("/opcionados", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/templates/lista_opcionados.html"));
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
